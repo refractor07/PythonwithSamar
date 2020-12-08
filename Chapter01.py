1a4716c4d6f5b621e2323fd60523f174256e17d2
samar <49747971+refractor07@users.noreply.github.com>
2020-12-08 13:46:00 +0530

    Add files via upload

---
+++ Chapter01.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+2+5
+
+
+# In[2]:
+
+
+98/3
+
+
+# In[3]:
+
+
+98%3
+
+
+# In[4]:
+
+
+5>3
+
+
+# In[6]:
+
+
+45239>3294
+
+
+# In[7]:
+
+
+23>89
+
+
+# In[11]:
+
+
+#print("Hello World")
+print("Hello World")
+
